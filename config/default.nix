d240d7cfd024429b0e4bf9cf0929c1034d799d4d
Per Johansson <per.a.johansson@svt.se>
2023-12-04 22:29:35 +0100

    try without

--- config/default.nix
+++ config/default.nix
@@ -9,41 +9,41 @@
 ./plugins.nix
   ];
 
-    extraPlugins = with pkgs.vimPlugins; [
-      lualine-lsp-progress
-      lspkind-nvim
-      luasnip
-      #   barbecue-nvim
-      #   diffview-nvim
-      #   feline-nvim
-      #   FixCursorHold-nvim
-      #   FTerm-nvim
-      # gitsigns-nvim
-      #   indent-blankline-nvim
-      #   instant-nvim
-      lazygit-nvim
-      markdown-preview-nvim
-      #   mini-nvim
-      #   mkdir-nvim
-      #   nui-nvim
-      # nvim-colorizer-lua
-      # nvim-lightbulb
-      #   nvim-navic
-      #   nvim-ts-rainbow2
-      #   nvim-web-devicons
-      #   octo-nvim
-      #   plenary-nvim
-      #   presence-nvim
-      #   renamer-nvim
-      #   telescope-ui-select-nvim
-      #   trouble-nvim
-      #   vim-cool
-      #   vim-smoothie
-      #   vim-unimpaired
-      #   vim-visual-multi
-      #   virtual-types-nvim
-      #   zen-mode-nvim
-    ];
+    # extraPlugins = with pkgs.vimPlugins; [
+    #   lualine-lsp-progress
+    #   lspkind-nvim
+    #   luasnip
+    #   #   barbecue-nvim
+    #   #   diffview-nvim
+    #   #   feline-nvim
+    #   #   FixCursorHold-nvim
+    #   #   FTerm-nvim
+    #   # gitsigns-nvim
+    #   #   indent-blankline-nvim
+    #   #   instant-nvim
+    #   lazygit-nvim
+    #   markdown-preview-nvim
+    #   #   mini-nvim
+    #   #   mkdir-nvim
+    #   #   nui-nvim
+    #   # nvim-colorizer-lua
+    #   # nvim-lightbulb
+    #   #   nvim-navic
+    #   #   nvim-ts-rainbow2
+    #   #   nvim-web-devicons
+    #   #   octo-nvim
+    #   #   plenary-nvim
+    #   #   presence-nvim
+    #   #   renamer-nvim
+    #   #   telescope-ui-select-nvim
+    #   #   trouble-nvim
+    #   #   vim-cool
+    #   #   vim-smoothie
+    #   #   vim-unimpaired
+    #   #   vim-visual-multi
+    #   #   virtual-types-nvim
+    #   #   zen-mode-nvim
+    # ];
 
 
 }
